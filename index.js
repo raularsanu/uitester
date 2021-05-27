74bc57f232f45657f9ec6a34104b6937a515000c
Raul <email@email.com>
2021-05-27 16:37:21 +0300

    new commit

--- index.js
+++ index.js
@@ -8,7 +8,19 @@ const Grid = require('gridfs-stream');
 const app = express();
 
 const keys = require('./config/keys');
-const PORT = 5000;
+
+if(process.env.NODE_ENV === 'production'){
+
+    app.use(express.static('client/build'));
+
+    const path = require('path');
+    app.get('*',(req,res)=>{
+        res.sendFile(path.resolve(__dirname,'client','build','index.html'));
+    })
+
+}
+
+const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cookieSession({
