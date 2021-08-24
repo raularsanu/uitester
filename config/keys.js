dc94cab73951452bd9a4db059b6363467bfeb4b3
raularsanu <88882858+raularsanu@users.noreply.github.com>
2021-08-24 14:11:02 +0300

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-    cookieKeys:['dasfasfasf','asfasfasff'],
-    dbURL:'mongodb+srv://rauladmin:test123@firstcluster.v5ht7.mongodb.net/usertesting-dev?retryWrites=true&w=majority'
+    cookieKeys:PROCESS.ENV.COOKIE_KEYS,
+    dbURL:PROCESS.ENV.DB_URL
 }
